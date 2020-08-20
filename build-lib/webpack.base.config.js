1913480d084a0d4ba500707ef1192b27ddc46656
TAIMEI\junfeng.gao <junfeng.gao@mobilemd.cn>
2020-08-20 22:05:41 +0800

    添加.d.ts

--- build-lib/webpack.base.config.js
+++ build-lib/webpack.base.config.js
@@ -10,15 +10,25 @@ module.exports = {
     },
     externals: {
         'react': 'react',
+        'react-dom': 'react-dom',
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js'],
     },
     module: {
         rules: [
-            { 
-                test: /\.tsx?$/, 
-                loader: 'ts-loader' 
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            // 指定特定的ts编译配置，为了区分脚本的ts配置
+                            configFile: path.resolve(__dirname, '../tsconfig.build.json'),
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
             },
             {
                 test: /\.css$/,
